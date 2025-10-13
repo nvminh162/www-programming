8ac5da2deedb94f30a0fd61ee84d5aa9f3f8a00a
nvminh162 <nvminh162@gmail.com>
2025-10-13 20:58:21 +0700

    finish week07

---
+++ 01_lab/chapter04_springboot/22003405-nguyenvanminh-week07/src/main/java/com/nvminh162/__nguyenvanminh_week07/model/Comment.java
@@ -0,0 +1,2 @@
+package com.nvminh162.__nguyenvanminh_week07.model;public class Comment {
+}
---
+++ 01_lab/chapter04_springboot/22003405-nguyenvanminh-week07/src/main/java/com/nvminh162/__nguyenvanminh_week07/model/OrderLine.java
@@ -0,0 +1,17 @@
+package com.nvminh162.__nguyenvanminh_week07.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "order_lines")
+public class OrderLine {
+}
