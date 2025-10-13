2a2b55313ab41b8852d39071fb04e37274de58e2
nvminh162 <nvminh162@gmail.com>
2025-10-13 13:06:50 +0700

    push week07

---
+++ 01_lab/chapter04_springboot/22003405-nguyenvanminh-week07/src/test/java/com/nvminh162/__nguyenvanminh_week07/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvminh162.__nguyenvanminh_week07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
