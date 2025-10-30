3f6a0b8c9ae0147261af24532a2454f8025caa4d
Nguyen Van Minh <121565657+nvminh162@users.noreply.github.com>
2025-10-30 19:56:41 +0700

    create project spring ai

---
+++ 01_lab/chapter04_springboot/nguyenvanminh-spring-ai/src/main/java/com/nvminh162/springai/NguyenvanminhSpringAiApplication.java
@@ -0,0 +1,13 @@
+package com.nvminh162.springai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenvanminhSpringAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NguyenvanminhSpringAiApplication.class, args);
+    }
+
+}
---
+++ 01_lab/chapter04_springboot/nguyenvanminh-spring-ai/src/test/java/com/nvminh162/springai/NguyenvanminhSpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvminh162.springai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenvanminhSpringAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
