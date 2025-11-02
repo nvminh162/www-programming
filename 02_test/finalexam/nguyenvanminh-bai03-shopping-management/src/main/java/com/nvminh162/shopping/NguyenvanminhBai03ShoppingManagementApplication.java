cb6fc5cee640ddea4c11c7c35c23025b94a8bad6
nvminh162 <nvminh162@gmail.com>
2025-11-02 17:35:16 +0700

    push final practice test 03 - shopping management

---
+++ 02_test/finalexam/nguyenvanminh-bai03-shopping-management/src/main/java/com/nvminh162/shopping/NguyenvanminhBai03ShoppingManagementApplication.java
@@ -0,0 +1,13 @@
+package com.nvminh162.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenvanminhBai03ShoppingManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NguyenvanminhBai03ShoppingManagementApplication.class, args);
+    }
+
+}
---
+++ 02_test/finalexam/nguyenvanminh-bai03-shopping-management/src/test/java/com/nvminh162/shopping/NguyenvanminhBai03ShoppingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvminh162.shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenvanminhBai03ShoppingManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
