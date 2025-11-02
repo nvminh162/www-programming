cab893e464846f344da705dcb8d7b4efa655c4f1
nvminh162 <nvminh162@gmail.com>
2025-11-02 17:15:43 +0700

    push final practice test 02 - project only

---
+++ 02_test/finalexam/nguyenvanminh-bai02-course-management/src/main/java/com/nvminh162/course/NguyenvanminhBai02CourseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.nvminh162.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenvanminhBai02CourseManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NguyenvanminhBai02CourseManagementApplication.class, args);
+    }
+
+}
---
+++ 02_test/finalexam/nguyenvanminh-bai02-course-management/src/test/java/com/nvminh162/course/NguyenvanminhBai02CourseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvminh162.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenvanminhBai02CourseManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
